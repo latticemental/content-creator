701fdd8b5f487ab348a520b2366fc263fa421836
3vanDrop <evan.itf@gmail.com>
2025-06-12 20:51:13 -0700

    feat: updates on test_006

--- tests/tests_sanity/test_006_content_creator.py
+++ tests/tests_sanity/test_006_content_creator.py
@@ -1,5 +1,6 @@
 from tests.base_test import BaseTest
 from src.content_media_generator import ContentMediaGenerator_LongFormVideo
+from pathlib import Path
 
 
 class TestContentCreatorClass(BaseTest):
@@ -7,7 +8,11 @@ class TestContentCreatorClass(BaseTest):
     def test_content_creator_horizontal_usage(self):
         """ContentCreator Usage"""
         longform_video = ContentMediaGenerator_LongFormVideo(output_path="longform_video.mp4")
-        longform_video.create_audiobook(author="Jacobo Grinberg",
-                                        split_by_chapter=True,
-                                        video_loop="media_src/template.mp4",
-                                        duration=5*60)
+        output_video = longform_video.create_complete_audiobook(book="Teoria Sintergica",
+                                                                author="Jacobo Grinberg",
+                                                                video_loop=Path("media_src/template.mp4"),
+                                                                video_loop_len=14, # arg to be removed on future versions (video len will be calc automatically)
+                                                                duration=5*60,
+                                                                music_track=Path("media_src/music_track.mp3"))
+
+        assert self.file_exists(output_video), "Video shall exist!"
